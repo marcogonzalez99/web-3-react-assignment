7b4850962087043cc83586793c2d4a674cae77a7
Marco <mgonz781@mtroyal.ca>
2023-02-13 11:07:02 -0700

    image changes

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -3,12 +3,13 @@ import React from 'react';
 //import { Link } from 'react-router-dom';
 
 const Home = props => {
-    let imgURL = 'https://unsplash.com/photos/X93I1fRX1QE';
+    let imgURL = 'https://images.unsplash.com/photo-1653022860307-0ccb6379f78b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80';
     return (
     <div className='banner'
         style={{
             backgroundImage: `url(${imgURL})`,
-            height: '800px',
+            height: '895px',
+            backgroundSize: 'cover',
             backgroundPosition: 'center center',
             backgroundRepeat: 'no-repeat'
     }}>
