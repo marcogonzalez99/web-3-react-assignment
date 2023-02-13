1dc8682185be4f0004bc18c40ae45718eb52e0ef
marcogonzalez99 <Mgonz781@mtroyal.ca>
2023-02-13 14:47:50 -0700

    modules

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Home = props => {
     let imgURL = 'https://images.unsplash.com/photo-1653022860307-0ccb6379f78b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80';
     return (
-    <div className='banner'
+    <div className='banner text-white font-bold text-center'
         style={{
             backgroundImage: `url(${imgURL})`,
             height: '895px',
@@ -13,9 +13,13 @@ const Home = props => {
             backgroundPosition: 'center center',
             backgroundRepeat: 'no-repeat'
     }}>
-        <div className='box-border h-320 w-320 bg-midnight'>
+        <div className='h-320 w-320 bg-slate'>
                 <h1>Movie Browser</h1>
+                <input type="text" placeholder="Enter search term" className="bg-black p-2 w-340 rounded-lg" />
+                <button className="bg-indigo-500">Show Matching Movies</button>
+                <button className='bg-indigo-500'>Show All Movies</button>
         </div>
+        <p className='fixed bottom-0 text-center p-4 bg-indigo-500 text-white'>Image Retreived From Unsplash, Author: </p>
     </div>  
     );
 }
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
 
-// Retreived from Tailwind CSS Site
+// Retreived from Tailwind CSS Site: https://tailwindcss.com/docs/customizing-colors
 module.exports = {
   theme: {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
       'white': '#ffffff',
+      'slate': '#64748b',
       'purple': '#3f3cbb',
       'midnight': '#121063',
       'metal': '#565584',
