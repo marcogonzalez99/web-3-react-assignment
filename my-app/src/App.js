00bbb18cadd44e7299dc5e952c28a026a30593ae
Marco <mgonz781@mtroyal.ca>
2023-02-13 10:01:45 -0700

    more changes to home and app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,40 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-//import HeaderApp from './components/HeaderApp';
+// Import react
+import React, {useEffect, useState} from 'react';
+// Importing Components
+import HeaderApp from './components/HeaderApp';
 import Home from './components/Home';
+// Importing Lodash for deepclones
+import * as cloneDeep from 'lodash/cloneDeep';
+// Importing Route for links
+import { Route } from 'react-router-dom';
 
 function App() {
+  // For the movies state
+  const [movies, setMovies] = useState([]);
+
+  // Function to retrive the movie data
+  useEffect( () => {
+    const getData = async () => {
+    try { 
+      const url = "";
+      const response = await fetch(url);
+      const data = await response.json();
+      setMovies(data);
+    }
+    catch (err) {
+      console.error(err);
+    }
+  };
+  getData();
+  }, [] );
+
+
+  // What we are rendering for the user
   return (
     <main>
+      <HeaderApp />
       <Home />
     </main>
   );
---
+++ my-app/src/components/Home.js
@@ -0,0 +1,15 @@
+// Importing React
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = props => {
+    let imgURL = "../home_image.jpg";
+
+    return (
+    <div>
+        <h1>Hello Home Page</h1>
+    </div>  
+    );
+}
+
+export default Home;
