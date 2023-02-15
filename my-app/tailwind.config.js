12921323376a468d31bf90862ef4c35970225370
Marco <mgonz781@mtroyal.ca>
2023-02-15 08:55:00 -0700

    branch with working tailwind css stuff

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,27 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
-// Retreived from Tailwind CSS Site: https://tailwindcss.com/docs/customizing-colors
-module.exports = {
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'slate': '#64748b',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-    },
-  },
 }
