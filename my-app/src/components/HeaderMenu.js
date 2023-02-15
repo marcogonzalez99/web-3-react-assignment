1bb4322805ea14e5ec2d31e7582db061f7514dfe
Marco <mgonz781@mtroyal.ca>
2023-02-15 16:07:45 -0700

    css more

--- my-app/src/components/HeaderMenu.js
+++ my-app/src/components/HeaderMenu.js
@@ -3,8 +3,8 @@ import React from 'react';
 const HeaderMenu = function (props) {
     return (
         <nav className='absolute inset-y-4 right-0 p-3'>
-            <button className='text-xl bg-[#3aafa9] mx-2 px-5 rounded'>Home</button>
-            <button className='text-xl bg-[#3aafa9] mx-2 px-5 rounded'>All Movies</button>
+            <button className='text-xl bg-[#3aafa9] mx-2 px-5 rounded hover:bg-sky-700'>Home</button>
+            <button className='text-xl bg-[#3aafa9] mx-2 px-5 rounded hover:bg-sky-700'>All Movies</button>
         </nav>
     );
 }
--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -16,8 +16,8 @@ const Home = props => {
         <div className='bg-slate-300 w-2/5 h-1/3 content-center mx-auto my-auto rounded-lg py-5 relative top-1/4'>
             <h1 className='text-black text-3xl py-5'>Movie Browser</h1>
             <input className="bg-black p-2 w-3/4 rounded-md mx-10 my-8 py-3" type="text" placeholder="Enter search term"/>
-            <button className="text-xl bg-[#3aafa9] mx-2 px-5 py-1 rounded">Show Matching Movies</button>
-            <button className='text-xl bg-[#3aafa9] mx-2 px-5 py-1 rounded'>Show All Movies</button>
+            <button className="text-xl bg-[#3aafa9] mx-2 px-5 py-1 rounded hover:bg-sky-700">Show Matching Movies</button>
+            <button className="text-xl bg-[#3aafa9] mx-2 px-5 py-1 rounded hover:bg-sky-700">Show All Movies</button>
         </div>
         <p className='fixed bottom-0 text-center p-4 text-white'>Image Retreived From Unsplash, Author: </p>
     </div>  
