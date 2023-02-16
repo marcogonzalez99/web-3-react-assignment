17f8286685393d736fa8d8a1646b46b43e979a87
marcogonzalez99 <Mgonz781@mtroyal.ca>
2023-02-15 22:53:53 -0700

    pain and suffering

--- my-app/src/components/DefaultView_Filter.js
+++ my-app/src/components/DefaultView_Filter.js
@@ -9,19 +9,19 @@ const DefaultView_Filter = props => {
     const [submit, setSubmit] = React.useState("0");
 
     return (
-     <form className ="viewFilter">
-         <input type="radio" value="Title" onChange={onValueChange} checked={"Title" === filter}></input>
-         <label>Title</label>
-         <input type="text" disabled ="yes"></input>
-         <input type="radio" value="Genre" onChange={onValueChange} checked={"Genre" === filter}></input>
-         <label>Genre</label>
-         <input type="radio" value="Year" onChange={onValueChange} checked={"Year" === filter}></input>
-         <label>Year</label>
-         <input type="radio" value="Rating" onChange={onValueChange} checked={"Rating" === filter}></input>
-         <label>Rating</label>
-         <button>Submit</button>
-         <button>Clear</button>
-     </form>
+    <form className ="viewFilter">
+    {/* //      <input type="radio" value="Title" onChange={onValueChange} checked={"Title" === filter}></input>
+    //      <label>Title</label>
+    //      <input type="text" disabled ="yes"></input>
+    //      <input type="radio" value="Genre" onChange={onValueChange} checked={"Genre" === filter}></input>
+    //      <label>Genre</label>
+    //      <input type="radio" value="Year" onChange={onValueChange} checked={"Year" === filter}></input>
+    //      <label>Year</label>
+    //      <input type="radio" value="Rating" onChange={onValueChange} checked={"Rating" === filter}></input>
+    //      <label>Rating</label>
+    //      <button>Submit</button>
+    //      <button>Clear</button> */}
+    </form>
     )
 }
 
--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -3,24 +3,24 @@ import React from 'react';
 //import { Link } from 'react-router-dom';
 
 const Home = props => {
-    let imgURL = 'https://images.unsplash.com/photo-1653022860307-0ccb6379f78b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80';
+
     return (
-    <div className='banner text-white font-bold text-center'
+        <div className="text-white text-center bg-hero bg-cover bg-center"     
         style={{
-            backgroundImage: `url(${imgURL})`,
-            height: '891px',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center center',
-            backgroundRepeat: 'no-repeat'
-    }}>
-        <div className='bg-slate-300 w-2/5 h-1/3 content-center mx-auto my-auto rounded-lg py-5 relative top-1/4'>
-            <h1 className='text-black text-3xl py-5'>Movie Browser</h1>
-            <input className="bg-black p-2 w-3/4 rounded-md mx-10 my-8 py-3" type="text" placeholder="Enter search term"/>
-            <button className="text-xl bg-[#3aafa9] mx-2 px-5 py-1 rounded hover:bg-sky-700">Show Matching Movies</button>
-            <button className="text-xl bg-[#3aafa9] mx-2 px-5 py-1 rounded hover:bg-sky-700">Show All Movies</button>
-        </div>
-        <p className='fixed bottom-0 text-center p-4 text-white'>Image Retreived From Unsplash, Author: https://unsplash.com/@thehalaldesign</p>
-    </div>  
+        height: '891px',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        backgroundRepeat: 'no-repeat'
+        }}
+    >
+            <div className='bg-slate-300 w-2/5 h-1/3 content-center mx-auto my-auto rounded-lg py-5 relative top-1/4'>
+                <h1 className='text-black text-3xl py-5'>Movie Browser</h1>
+                <input className="bg-black p-2 w-3/4 rounded-md mx-10 my-8 py-3" type="text" placeholder="Enter search term"/>
+                <button className="text-xl bg-[#3aafa9] mx-2 px-5 py-1 rounded hover:bg-sky-700">Show Matching Movies</button>
+                <button className="text-xl bg-[#3aafa9] mx-2 px-5 py-1 rounded hover:bg-sky-700">Show All Movies</button>
+            </div>
+                <p className='fixed bottom-0 text-center p-4 text-white'>Image Retreived From Unsplash, Author: https://unsplash.com/@thehalaldesign</p>
+        </div> 
     );
 }
 
