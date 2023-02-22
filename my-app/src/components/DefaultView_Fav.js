647dc7c67057cf110f743692589184488faf2c27
marcogonzalez99 <Mgonz781@mtroyal.ca>
2023-02-22 12:25:31 -0700

    addded the X to the screen on hover of a movie image

--- my-app/src/components/DefaultView_Fav.js
+++ my-app/src/components/DefaultView_Fav.js
@@ -1,15 +1,33 @@
 // Import React
-import React from 'react';
+import React, { useState} from 'react';
 const DefaultViewFav = props => {
     const imgURL = `https://image.tmdb.org/t/p/w92`
+    const [hover, setHover] = useState(false);
+
+    const handleMouseEnter = () => {
+        setHover(true);
+    };
+    
+    const handleMouseLeave = () => {
+        setHover(false);
+    };
+
+
     return (
      <div className='h-screen overflow-y-scroll'>
         <h1 className='text-3xl font-bold text-center pt-5 mb-5'>Favourites List</h1>
         {props.favourited.map( (m,index) => 
         
         <div key={index}>
-            <div className='px-4 py-3 my-3 mx-3 flex items-center bg-gray-300 rounded'>
-                <span><img src={`${imgURL}${m.poster}`} alt={m.title}></img></span>
+            <div className='px-4 py-3 my-3 mx-3 flex items-center bg-gray-300 rounded relative' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <span>
+                    <img className='relative inline-block' src={`${imgURL}${m.poster}`} alt={m.title} ></img>
+                    {hover && (
+                        <button className="absolute top-0 right-0 p-2 text-black rounded-full">
+                        X
+                        </button>
+                    )}
+                </span>
                 <span className='text-xl font-bold px-2'><h1>{m.title}</h1></span>
             </div>
             
